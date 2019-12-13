4f34f98e236f81d3a832ceca20e690acc7a4a506
Philipp Kreutzer <p.kreutzer@bucs-it.de>
2019-12-13 09:52:09 +0100

    cleanup init scripts

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -4,4 +4,29 @@ is_ubuntu || return 1
 # Update APT.
 e_header "Updating APT"
 sudo apt-get -qq update
-sudo apt-get -qq upgrade
+sudo apt-get -qq dist-upgrade
+
+# Install APT packages.
+packages=(
+    build-essential
+    cowsay
+    git-core
+    htop
+    libssl-dev
+    silversearcher-ag
+    tree
+    tig
+    neovim
+    autojump
+    unzip
+)
+
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+
+if (( ${#packages[@]} > 0 )); then
+    e_header "Installing APT packages: ${packages[*]}"
+    for package in "${packages[@]}"; do
+        sudo apt-get -qq install "$package"
+    done
+fi
+
