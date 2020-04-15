bd2d4b585818ff2d7401cfa05ae60879d7a891c5
Philipp <p.kreutzer@bucs-it.de>
2020-04-15 10:42:20 +0200

    Add check to vim-plug installation

--- init/50_vim-plug.sh
+++ init/50_vim-plug.sh
@@ -1,1 +1,4 @@
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [[ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]]; then
+    e_header "Installing vim-plug for neovim"
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi 
