6c8818643f82b5d8c6d9871aa8385dfa878c8435
Philipp <p.kreutzer@bucs-it.de>
2020-10-24 23:46:42 +0200

    Add installation for fisher

--- init/50_fisher.sh
+++ init/50_fisher.sh
@@ -1,3 +1,11 @@
+if [[ ! -f ~/.config/fish/functions/fisher.fish ]]; then
+    e_header "Fisher is not installed"
+    read -n 1 -t $prompt_delay -p "Install fisher to ~/.config/fish/? [y/N] " install_fisher; echo
+    if [[ "$install_fisher" =~ [Yy] ]]; then
+        curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+    fi
+fi
+
 if [[ -f ~/.config/fish/functions/fisher.fish ]]; then
     e_header "Updating fisher"
     fish -c "fisher self-update"
