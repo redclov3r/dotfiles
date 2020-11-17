e71ac6bc91a4f744f3ff1f777871906ba5a2a37f
Philipp <p.kreutzer@bucs-it.de>
2020-11-17 11:59:07 +0100

    get ready for new version of fisher

--- init/40_fish_links.sh
+++ init/40_fish_links.sh
@@ -1,6 +1,6 @@
 read -n 1 -t $prompt_delay -p "Link config.fish, fishfile and fish_variables to ~/.config/fish/? [y/N] " link_fish; echo
 if [[ "$link_fish" =~ [Yy] ]]; then
     ln -s ~/.fish/config.fish ~/.config/fish/config.fish
-    ln -s ~/.fish/fishfile ~/.config/fish/fishfile
+    ln -s ~/.fish/fish_plugins ~/.config/fish/fish_plugins
     ln -s ~/.fish/fish_variables ~/.config/fish/fish_variables
 fi
--- init/50_fisher.sh
+++ init/50_fisher.sh
@@ -7,9 +7,6 @@ if [[ ! -f ~/.config/fish/functions/fisher.fish ]]; then
 fi
 
 if [[ -f ~/.config/fish/functions/fisher.fish ]]; then
-    e_header "Updating fisher"
-    fish -c "fisher self-update"
-
     e_header "Updating fisher packages"
-    fish -c "fisher"
+    fish -c "fisher update"
 fi
